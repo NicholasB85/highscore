953116617580d58e33e28331220ec4b5592f0959
NicholasB85 <nicholas.w.burkhart@gmail.com>
2018-06-28 14:16:23 -0400

    express first commit

--- scores.js
+++ scores.js
@@ -1,56 +1,83 @@
-const http = require('http');
-const textBody = require("body");
+const express = require('express');
+const app = express();
+// const textBody = require("body");
 const jsonBody = require("body/json");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
 var scores = [{ name: "Edwin", score: 40 }, { name: "Billy the Kid", score: 39 }];
 const resources = { "/scores": JSON.stringify(scores) };
 
-const server = http.createServer((req, res) => {
 
-    if (req.method === "GET") {
-        if (resources[req.url] === undefined) {
-            res.statusCode = 404;
-            res.end("ERROR NOT FOUND");
-        } else {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            const responseBody = JSON.stringify(scores)
-            res.end(responseBody);
-        }
-    } else if (req.method === "PUT") {
-        res.statusCode = 201;
-        textBody(req, res, (err, requestBody) => {
-            resources[req.url] = requestBody;
-            const responseBody = resources[req.url];
+app.get('/scores', function (req, res){ 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    const responseBody = JSON.stringify(scores)
+    res.end(responseBody);
+    
+})            
+app.get('/*', function (req, res){ res.statusCode = 404;
+            res.end("ERROR NOT FOUND")})
 
-
-            res.end(responseBody);
-        })
-
-    } else if (req.method === "POST") {
-        res.statusCode = 201;
+app.post('/scores', function (req, res) {
+    res.statusCode = 201;
         jsonBody(req, res, (err, requestBody) => {
             scores.push(requestBody)
-            console.log("req", req)
-            console.log("res", res)
-            console.log("request body", requestBody)
             scores.sort((a, b) => b.score - a.score);
             scores = scores.slice(0, 3)
             resources[req.url] = JSON.stringify(scores);
             const responseBody = resources[req.url];
             res.end(responseBody);
-        })
-    }
-
-
+})
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
 
-});
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// const server = http.createServer((req, res) => {
+
+//     if (req.method === "GET") {
+//         if (resources[req.url] === undefined) {
+//             res.statusCode = 404;
+//             res.end("ERROR NOT FOUND");
+//         } else {
+//             res.statusCode = 200;
+//             res.setHeader('Content-Type', 'application/json');
+//             const responseBody = JSON.stringify(scores)
+//             res.end(responseBody);
+//         }
+//     } else if (req.method === "PUT") {
+//         res.statusCode = 201;
+//         textBody(req, res, (err, requestBody) => {
+//             resources[req.url] = requestBody;
+//             const responseBody = resources[req.url];
+
+
+//             res.end(responseBody);
+//         })
+
+//     } else if (req.method === "POST") {
+//         res.statusCode = 201;
+//         jsonBody(req, res, (err, requestBody) => {
+//             scores.push(requestBody)
+//             console.log("req", req)
+//             console.log("res", res)
+//             console.log("request body", requestBody)
+//             scores.sort((a, b) => b.score - a.score);
+//             scores = scores.slice(0, 3)
+//             resources[req.url] = JSON.stringify(scores);
+//             const responseBody = resources[req.url];
+//             res.end(responseBody);
+//         })
+//     }
+
+
+// // });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+
+// });
 
 
